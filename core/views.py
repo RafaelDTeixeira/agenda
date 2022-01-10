6ff1ede4de4e91c401ae58d9761f9f5deee4d2f5
Rafael Teixeira <rafaeldtx@hotmail.com>
2022-01-10 19:40:46 -0300

    Implementação de rotas das tabelas

--- core/views.py
+++ core/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render,HttpResponse
 
-# Create your views here.
+from core.models import Evento
+
+def agendamento(resquest):
+    evento=Evento.objects.all()
+    dados={'eventos':evento}
+    return render(resquest,'agenda.html',dados)
+
+def titulo(resquest,titulo_evento):
+    consulta=Evento.objects.get(titulo=titulo_evento)
+    dados={'consulta':consulta}
+    return render(resquest,'titulo.html',dados)
