8248e45c9b17078abab91e9766233378bd35cbd0
Rafael Teixeira <rafaeldtx@hotmail.com>
2022-01-10 13:47:09 -0300

    Criação de Classes/Config Admin

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from core.models import Evento, Pessoa
 
-# Register your models here.
+@admin.register(Evento)
+class EventoAdmin(admin.ModelAdmin):
+    list_display=('titulo','data_evento','data_criacao')
+
+@admin.register(Pessoa)  
+class PessoaAdmin(admin.ModelAdmin):
+    list_display=('nome','idade','rg','endereco')
+
--- core/models.py
+++ core/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.db.models.fields import DateTimeField
 
-# Create your models here.
+class Evento(models.Model):
+    titulo=models.CharField(max_length=100)
+    descricao=models.TextField(null=True, blank=True)
+    data_evento=DateTimeField(verbose_name='Data do Evento')
+    data_criacao=DateTimeField(auto_now=True,verbose_name='Data de Criação')
+
+    class Meta:                 #Alteração do nome da tabela sqlite3: comando sqlmigrate core xxxx/dados para migração manual.
+        db_table='evento'
+
+    def __str__(self) -> str:
+        return self.titulo
+
+class Pessoa(models.Model):
+    nome=models.CharField(max_length=100,null=True)
+    idade=models.IntegerField()
+    rg=models.CharField(max_length=10)
+    endereco=models.TextField(max_length=120,null=True)
+
+    def __str__(self) -> str:
+        return self.nome
