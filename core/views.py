5baf165b6dcd22c0c46cb491e9a4cf82b6861601
Rafael Teixeira <rafaeldtx@hotmail.com>
2022-01-11 16:04:51 -0300

    add query / layout

--- core/views.py
+++ core/views.py
@@ -1,13 +1,21 @@
+from sys import displayhook
 from django.shortcuts import render,HttpResponse
 
 from core.models import Evento
 
 def agendamento(resquest):
-    evento=Evento.objects.all()
-    dados={'eventos':evento}
+    usuario=resquest.user
+    evento=Evento.objects.filter(usuario=usuario)
+    usuario=str(usuario).capitalize
+    
+    dados={'eventos':evento,'usuario':usuario}
     return render(resquest,'agenda.html',dados)
 
 def titulo(resquest,titulo_evento):
-    consulta=Evento.objects.get(titulo=titulo_evento)
-    dados={'consulta':consulta}
+    print(titulo_evento)
+    usuario=resquest.user
+    consulta=Evento.objects.get(titulo__contains=titulo_evento)
+    usuario=str(usuario).capitalize
+
+    dados={'consulta':consulta,'usuario':usuario}
     return render(resquest,'titulo.html',dados)
